ce86fd9c9a8e5a91bd594fb0423ac6bcb31f0e6f
WytheHuang <zxc22143579.wythe@gmail.com>
2023-12-11 13:40:56 +0800

    update

--- frontend_streamlit/Home.py
+++ frontend_streamlit/Home.py
@@ -236,14 +236,14 @@ else:
                 st.button(
                     "Archive",
                     key="move_archive",
-                    disabled=True if st.session_state["notebooks"]["options_id"] == "archive" else False,
+                    disabled=True if st.session_state["notes"]["is_archive"] else False,  # type: ignore
                     on_click=lambda: utils.move_note_to_archive(st.session_state["token"], id),  # type: ignore
                 )
             with sub_col2:
                 st.button(
                     "Move to Trash",
                     key="move_trash",
-                    disabled=True if st.session_state["notebooks"]["options_id"] == "trash" else False,
+                    disabled=True if st.session_state["notes"]["is_archive"] == "trash" else False,
                     on_click=lambda: utils.move_note_to_trash(st.session_state["token"], id),  # type: ignore
                 )  # type: ignore
             with sub_col3:
